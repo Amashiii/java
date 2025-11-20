cc43c0f5271de972f2c4c8be3c4d90bd7a479f35
Amashiii <stolarczykpawel46@gmail.com>
2025-11-20 18:28:53 +0100

    java

--- PodstawyJavy/src/Main.java
+++ PodstawyJavy/src/Main.java
@@ -1,9 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-    // zad.1
-        double stopnieCelcjusza, stopnieFahrenhaita
-        }
+    public static void main(String[] args)
     }
 }
